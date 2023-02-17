e84564e1079447bcaa139e048b5ca95e0d36cbdc
Brenda <brendacamano@gmail.com>
2023-02-17 15:30:53 -0300

    finalizado

---
+++ Js/JS.js
@@ -0,0 +1,30 @@
+var menu_visible = false;
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu(){
+    if(menu_visible==false){//si esta oculto
+    menu.style.display = "block";
+    menu_visible = true;
+}
+  else{
+    menu.style.display = "none";
+    menu_visible = false;
+  }
+}
+
+let links = document.querySelectorAll("nav a")
+for(var x = 0; x <links.length;x++){
+    links[x].onclick = function(){
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+
+
+function creaeBarra(id_barra){
+    for(i=0;i<16;i++){
+        let div = document.createElement("div");
+        div.className = "e";
+        id_barra.appendChild(div);
+    }
+}
+
